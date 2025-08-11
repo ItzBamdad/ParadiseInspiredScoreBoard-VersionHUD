357667ea35d12587edd04fc268b3a082dcb311e3
! 𝕀𝕥𝕫𝔹𝕒𝕞𝕕𝕒𝕕 :)🎈 <itzbamdad@gmail.com>
2025-08-11 09:53:45 +0330

    Config | کانفیگ و تنظیمات

---
+++ config.lua
@@ -0,0 +1,96 @@
+-- Ui BY: Mini Sara
+-- ReCoded By: ItzBamdad
+Config = {}
+
+
+-- تنظیمات نیرو های امنیتی مورد نیاز برای هر رابری | Robbery Icon Settings: Jobs Required
+Config.Robs = {
+    ["vehiclerob"] = {
+        requiredJobs = {
+            police = 2,
+            sheriff = 1
+        }
+    },
+    ["shop"] = {
+        requiredJobs = {
+            police = 1
+        }
+    },
+    ["karaj"] = {
+        requiredJobs = {
+            police = 2
+        }
+    },
+    ["cartel"] = {
+        requiredJobs = {
+            police = 3,
+            fib = 1
+        }
+    },
+    ["chicken"] = {
+        requiredJobs = {
+            police = 1
+        }
+    },
+    ["javaheriPaleto"] = {
+        requiredJobs = {
+            police = 2
+        }
+    },
+    ["miniBank"] = {
+        requiredJobs = {
+            police = 2,
+            sheriff = 1
+        }
+    },
+    ["javaheri"] = {
+        requiredJobs = {
+            police = 3
+        }
+    },
+    ["yatch"] = {
+        requiredJobs = {
+            police = 3,
+            coastguard = 1
+        }
+    },
+    ["jr"] = {
+        requiredJobs = {
+            police = 1
+        }
+    },
+    ["farm"] = {
+        requiredJobs = {
+            police = 1
+        }
+    },
+    ["merryweather"] = {
+        requiredJobs = {
+            police = 2,
+            army = 1
+        }
+    },
+    ["shop_ru"] = {
+        requiredJobs = {
+            police = 1
+        }
+    },
+    ["shop_fr"] = {
+        requiredJobs = {
+            police = 1
+        }
+    },
+    ["shop_tw"] = {
+        requiredJobs = {
+            police = 1
+        }
+    },
+    ["shop_co"] = {
+        requiredJobs = {
+            police = 1
+        }
+    }
+}
+
+-- زمان به‌روزرسانی دیتا (ثانیه) | Update Config
+Config.UpdateInterval = 5
